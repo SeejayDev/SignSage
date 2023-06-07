0feeb655c2517ec69cd538ee3034757495789508
SeejayDev <seejaydev@gmail.com>
2023-06-07 12:42:13 +0800

    7 june

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import { firebase_auth } from "@firebase/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from 'react'
 
 const index = () => {
+  const [error, setError] = useState('')
 
   const submitForm = async (e) => {
     e.preventDefault()
 
+    // get data from form object
     const formData = new FormData(e.target)
     const payload = Object.fromEntries(formData)
 
-    console.log(payload)
+    const { email, password } = payload
+    if (password.length < 6) {
+      setError("Password must be at least 6 characters!")
+      return
+    }
+    if (password !== payload["confirm-password"]) {
+      setError("Passwords do not match!")
+      return
+    }
+
+    // Create the new account
+    createUserWithEmailAndPassword(firebase_auth, payload.email, payload.password)
+    .then((credentials) => {
+      console.log(credentials)
+    })
+    .catch((error) => {
+      console.log(error.message)
+    })
   }
 
   return (
@@ -19,15 +40,19 @@ const index = () => {
         </div>
 
         <div className='w-1/2'>
+          <p>{error}</p>
           <form onSubmit={submitForm}>
+            <p>Username:</p>
+            <input type="text" name="username" />
+
             <p>Email:</p>
-            <input type="text" id="first" name="first" />
+            <input type="text" name="email" />
 
             <p>Password:</p>
-            <input type="password" id="last" name="last" />
+            <input type="password" name="password" />
 
             <p>Confirm Password:</p>
-            <input type="password"/>
+            <input type="password" name="confirm-password"/>
 
             <button type="submit">Submit</button>
           </form>
